96f2f6f01d09fe6f781f522773c36806300ef6e3
Paul Battley <pbattley@gmail.com>
2026-09-15 16:35:52 +0100

    Normalise test file indentation

--- src/test/java/uk/sanshinkai/kickstosvg/FuriganaStringParserTest.java
+++ src/test/java/uk/sanshinkai/kickstosvg/FuriganaStringParserTest.java
@@ -4,46 +4,46 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FuriganaStringParserTest {
-  @Test
-  public void testEmptyString() {
-    var parsed = new FuriganaStringParser().parse("");
+    @Test
+    public void testEmptyString() {
+        var parsed = new FuriganaStringParser().parse("");
 
-    assertTrue(parsed.isEmpty());
-  }
+        assertTrue(parsed.isEmpty());
+    }
 
-  @Test
-  public void testSimpleString() {
-    var parsed = new FuriganaStringParser().parse("ABC");
+    @Test
+    public void testSimpleString() {
+        var parsed = new FuriganaStringParser().parse("ABC");
 
-    assertEquals(1, parsed.size());
+        assertEquals(1, parsed.size());
 
-    assertEquals("ABC", parsed.get(0).surface());
-    assertNull(parsed.get(0).reading());
-  }
+        assertEquals("ABC", parsed.get(0).surface());
+        assertNull(parsed.get(0).reading());
+    }
 
-  @Test
-  public void testStringWithReading() {
-    var parsed = new FuriganaStringParser().parse("{ABC}{DEF}");
+    @Test
+    public void testStringWithReading() {
+        var parsed = new FuriganaStringParser().parse("{ABC}{DEF}");
 
-    assertEquals(1, parsed.size());
+        assertEquals(1, parsed.size());
 
-    assertEquals("ABC", parsed.get(0).surface());
-    assertEquals("DEF", parsed.get(0).reading());
-  }
+        assertEquals("ABC", parsed.get(0).surface());
+        assertEquals("DEF", parsed.get(0).reading());
+    }
 
-  @Test
-  public void testComplexString() {
-    var parsed = new FuriganaStringParser().parse("AA{BB}{CC}DD");
+    @Test
+    public void testComplexString() {
+        var parsed = new FuriganaStringParser().parse("AA{BB}{CC}DD");
 
-    assertEquals(3, parsed.size());
+        assertEquals(3, parsed.size());
 
-    assertEquals("AA", parsed.get(0).surface());
-    assertNull(parsed.get(0).reading());
+        assertEquals("AA", parsed.get(0).surface());
+        assertNull(parsed.get(0).reading());
 
-    assertEquals("BB", parsed.get(1).surface());
-    assertEquals("CC", parsed.get(1).reading());
+        assertEquals("BB", parsed.get(1).surface());
+        assertEquals("CC", parsed.get(1).reading());
 
-    assertEquals("DD", parsed.get(2).surface());
-    assertNull(parsed.get(2).reading());
-  }
+        assertEquals("DD", parsed.get(2).surface());
+        assertNull(parsed.get(2).reading());
+    }
 }
